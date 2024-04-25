46b5beba2105fc9c6ab54c618e5fc310a105c880
Balamuruganraja <balamuruganraja0106@gmail.com>
2024-04-25 17:37:30 +0530

    keeper app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//1. Create a new React app.
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import "./styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
